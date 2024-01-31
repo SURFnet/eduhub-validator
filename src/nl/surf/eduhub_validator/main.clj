3a9ab5d68823d2ae867b566e006c4b862e708d8c
Joost Diepenmaat <joost@zeekat.nl>
2024-01-31 14:16:06 +0100

    Print included openapi spec versions with app version

--- src/nl/surf/eduhub_validator/main.clj
+++ src/nl/surf/eduhub_validator/main.clj
@@ -1,5 +1,6 @@
 (ns nl.surf.eduhub-validator.main
-  (:require [clojure.java.io :as io]
+  (:require [babashka.json :as json]
+            [clojure.java.io :as io]
             [clojure.string :as string]
             [clojure.tools.cli :refer [parse-opts]]
             [nl.jomco.apie.main :as apie]))
@@ -21,18 +22,37 @@
        option))
    apie/cli-options))
 
-(defn version
-  "Return app version"
+(defn app-version
+  "Return version of app."
   []
   (when-let [r (io/resource "nl/surf/eduhub_validator/version.txt")]
     (string/trim (slurp r))))
 
+(defn spec-version
+  "Return version of openapi spec for built-in profile."
+  [profile]
+  (-> profile
+      apie/file-or-resource
+      apie/read-edn
+      :openapi-spec
+      apie/file-or-resource
+      apie/read-json
+      (get-in ["info" "version"])))
+
+(defn versions
+  "Return version of app and included openapi specs."
+  []
+  (str "eduhub-validator " (app-version) "\n"
+       "included profiles:\n"
+       (string/join "\n" (map (fn [p]
+                                (str " - " p " " (spec-version p)))
+                              included-profiles))))
 
 (defn -main
   [& args]
   (let [{:keys [errors summary options]} (parse-opts args cli-options)]
     (when (:print-version? options)
-      (println (version))
+      (println (versions))
       (System/exit 0))
     (when (seq errors)
       (run! println errors)
